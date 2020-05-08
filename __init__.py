d45166553d8465d816f850de188f5b81b9b3b61d
crescents720 <44968100+crescents720@users.noreply.github.com>
2020-05-08 20:48:09 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -3,5 +3,9 @@ from app import app
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
 
+def xyz():
+    if __name__ == '__main__':
+        app.run(debug=True, port=3000)
+
 def getApp():
-    return app
+    return xyz
